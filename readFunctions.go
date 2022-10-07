7e6eeed399629f2bfebb3489bbfdf6f7eb75f409
Jan Eike Suchard <jan-eike.suchard@magenta.de>
2022-10-07 10:54:06 +0200

    Added function for reading the configuration of a service

--- readFunctions.go
+++ readFunctions.go
@@ -50,3 +50,41 @@ func ReadUpstreamInformation(upstreamName string) (*UpstreamConfiguration, error
 		return nil, errors.New("unexpected http status")
 	}
 }
+
+func ReadServiceConfiguration(serviceName string) (*ServiceConfiguration, error) {
+	if gatewayAPIURL == "" {
+		return nil, errors.New("the connection to the api gateway was not set up")
+	}
+	if serviceName == "" || strings.TrimSpace(serviceName) == "" {
+		return nil, errors.New("empty serviceName supplied")
+	}
+	// Create a new instance of the service configuration
+	serviceConfiguration := &ServiceConfiguration{}
+
+	// Make a request to the api gateway
+	response, err := http.Get(gatewayAPIURL + "/services/" + serviceName)
+	if err != nil {
+		logger.WithError(err).Error("An error occurred while sending the request to the api gateway")
+		return nil, err
+	}
+	switch response.StatusCode {
+	case 200:
+		jsonDecodeError := json.NewDecoder(response.Body).Decode(serviceConfiguration)
+		if jsonDecodeError != nil {
+			logger.WithError(jsonDecodeError).Error("Unable to parse the response sent by the api gateway")
+			return nil, jsonDecodeError
+		}
+		return serviceConfiguration, nil
+	case 400:
+		logger.WithField("httpCode", response.StatusCode).Error("A bad request was made to the api gateway")
+		return nil, errors.New("bad request sent to the gateway")
+	case 404:
+		logger.WithField("httpCode", response.StatusCode).Error(
+			"The supplied service name is not present in the api gateway")
+		return nil, errors.New("service not found")
+	default:
+		logger.WithFields(log.Fields{"httpCode": response.StatusCode,
+			"httpStatus": response.Status}).Error("An unexpected response code was received from the api gateway")
+		return nil, errors.New("unexpected response code")
+	}
+}
