383f285f2abdb052a407d8a3d18381d17b144fdb
Jan Eike Suchard <jan-eike.suchard@magenta.de>
2022-10-27 16:52:57 +0200

    Added documentation for function

--- helpers.go
+++ helpers.go
@@ -1,5 +1,6 @@
 package golang_kong_access
 
+// stringArrayContains checks if the string s is present in the string array a
 func stringArrayContains(a []string, s string) bool {
 	for _, item := range a {
 		if item == s {
